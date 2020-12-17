0a7025eaf551889af70defab7c3346dd669f8ec7
A <jesus.e.estrada@hotmail.com>
2020-12-17 12:26:56 -0600

    update

--- src/demonstration_2.py
+++ src/demonstration_2.py
@@ -11,6 +11,10 @@ The Stack class that you will use has been provided to you.
 class Stack:
     def __init__(self):
         self.data = []
+
+    #method to get the length of the stack
+    def __len__(self):
+      return len(self.data)
         
     def push(self, item):
         self.data.append(item)
@@ -21,14 +25,49 @@ class Stack:
         return "The stack is empty"
 
 class QueueTwoStacks:
-    def __init__(self):
-        # Your code here
+  def __init__(self):
+    #holds the elements in the in_stack in the reversed order
+    #when we call the dequeue method we'll pop from this atack
+    self.out_stack = Stack()
+    #holds the incoming enqueues elements
+    self.in_stack = Stack()
+  
+  def enqueue(self, item):
+    self.in_stack.push(item)
 
+  def dequeue(self):
+    #we need to check if the oustack is empty
+    #otherwise, just pop from the top of the `out_stack`
+    #if it is, we'll empty the `in_stack` into the `out_stack` in reverse order
+    if len(self.out_stack) == 0:
+      #empty the contents of the `in_stack` into the `out_stack`
+      while len(self.in_stack) != 0:
+        popped = self.in_stack.pop()
+        self.out_stack.push(popped)
+    
+    return self.out_stack.pop()
         
-    def enqueue(self, item):
-        # Your code here
+q = QueueTwoStacks()
+print(q.dequeue())
+q.enqueue(3)
+q.enqueue(6)
+print(q.dequeue())
+q.enqueue(7)
+q.enqueue(8)
+print(q.dequeue())
+print(q.dequeue())
+print(q.dequeue())
 
+# from collections import deque
 
-    def dequeue(self):
-        # Your code here
+# class QueueTwoStacks:
+#     def __init__(self):
+#       self.data = deque()
+        
+#     def enqueue(self, item):
+#       self.data.append(item)
 
+#     def dequeue(self):
+#       if len(self.data) > 0:
+#         return self.data.pop()
+#       return 'The stack is empty'
